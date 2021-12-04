e789f36568a1e243d133d32b156f81a1d455db26
Juan A. Moyano <wincus@gmail.com>
2021-12-04 20:07:36 -0300

    add day3 part 1 & 2

--- internal/common/curate.go
+++ internal/common/curate.go
@@ -2,6 +2,26 @@ package common
 
 import "strconv"
 
+func BinaryToDecimal(s []string) ([]int, error) {
+
+	var a []int
+
+	for _, v := range s {
+		if len(v) > 0 {
+			i, err := strconv.ParseInt(v, 2, 64)
+
+			if err != nil {
+				return a, err
+			}
+
+			a = append(a, int(i))
+		}
+	}
+
+	return a, nil
+
+}
+
 // ToInt converts a slice of strings
 // int a slice of Ints
 func ToInt(s []string) ([]int, error) {
--- internal/day3/utils.go
+++ internal/day3/utils.go
@@ -1,57 +1,178 @@
 package day3
 
 import (
+	"fmt"
 	"log"
 	"strconv"
 
 	"github.com/wincus/adventofcode2021/internal/common"
 )
 
-// Solve returns the solutions of Day3
+type popularity int
+
+const (
+	ZEROISTHEMOSTCOMMON popularity = iota
+	ONEISTHEMOSTCOMMON
+	EQUAL
+)
+
+type rate int
+
+const (
+	GAMMA rate = iota
+	EPSILON
+	OXYGEN
+	CO2
+)
+
+// Solve returns the solutions for day 3
 func Solve(s []string, p common.Part) int {
+
+	n := len(s[0])
+
+	r, err := common.BinaryToDecimal(s)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if p == common.Part1 {
+		gamma, err := getRate(r, n, GAMMA)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		epsilon, err := getRate(r, n, EPSILON)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		return gamma * epsilon
+	}
+
+	if p == common.Part2 {
+
+		oxygen, err := getRate(r, n, OXYGEN)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		co2, err := getRate(r, n, CO2)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		return oxygen * co2
+	}
+
+	log.Panicf("Unknown Part %v ", p)
+	return 0
+}
+
+func getRate(values []int, n int, r rate) (int, error) {
+
+	var result string
+
+	for i := n - 1; i >= 0; i-- {
+
+		// get the most common bit at index i
+		bit := getMostCommonBit(values, i)
+
+		switch r {
+		case GAMMA:
+			result = fmt.Sprintf("%v%v", result, bit)
+		case EPSILON:
+			result = fmt.Sprintf("%v%v", result, bit.Flip())
+		case OXYGEN:
+			if bit == EQUAL {
+				values = filterByBit(values, i, 1)
+			} else {
+				values = filterByBit(values, i, int(bit))
+			}
+		case CO2:
+			if bit == EQUAL {
+				values = filterByBit(values, i, 0)
+			} else {
+				values = filterByBit(values, i, int(bit.Flip()))
+			}
+		default:
+			return 0, fmt.Errorf("Unknown rate %v", r)
+		}
+
+		if r == OXYGEN || r == CO2 {
+			if len(values) == 1 {
+				return values[0], nil
+			}
+		}
+	}
+
+	// if we have gotten this far we have not found the rate
+	// for OXYGEN or CO2
+	if r == OXYGEN || r == CO2 {
+		return 0, fmt.Errorf("No value found for rate %v", r)
+	}
+
+	q, err := strconv.ParseInt(result, 2, 64)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return int(q), nil
+
+}
+
+// filterByBit removes from a slice of ints all elements where the the bit *not*
+// equal to the given bit at the given index.
+func filterByBit(r []int, index, bit int) []int {
+
+	var new []int
+
+	for _, v := range r {
+		if getBitByIndex(v, index) == bit {
+			new = append(new, v)
+		}
+	}
+
+	return new
+}
+
+// for a slice of ints s return which bit is most popular at a given index i
+func getMostCommonBit(s []int, i int) popularity {
 
 	var counter int
 
-	// diagnostic byte size
-	n := len(s[0])
-
-	gammaByte := make([]byte, n)
-	epsilonByte := make([]byte, n)
-
-	// assume that all entries have the same length
-	for i := 0; i < n; i++ {
-
-		counter = 0
-
-		for _, v := range s {
-			p, err := strconv.ParseInt(v, 2, 64)
-
-			if err != nil {
-				log.Panicf("could not convert binary: %v", err)
-			}
-			counter += (int(p) & (1 << i)) >> i
-		}
-
-		if counter > len(s)/2 {
-			gammaByte[n-i-1] = '1'
-			epsilonByte[n-i-1] = '0'
-		} else {
-			gammaByte[n-i-1] = '0'
-			epsilonByte[n-i-1] = '1'
-		}
+	for _, v := range s {
+		counter += getBitByIndex(v, i)
 	}
 
-	gamma, err := strconv.ParseInt(string(gammaByte), 2, 64)
-
-	if err != nil {
-		log.Panicf("could not convert binary: %v", err)
+	if 2*counter > len(s) {
+		return ONEISTHEMOSTCOMMON
 	}
 
-	epsilon, err := strconv.ParseInt(string(epsilonByte), 2, 64)
-
-	if err != nil {
-		log.Panicf("could not convert binary: %v", err)
+	if 2*counter < len(s) {
+		return ZEROISTHEMOSTCOMMON
 	}
 
-	return int(gamma) * int(epsilon)
+	return EQUAL
+}
+
+// getBitByIndex returns the bit value at the given index
+func getBitByIndex(v, i int) int {
+	return (v & (1 << i)) >> i
+}
+
+func (p popularity) Flip() popularity {
+	switch p {
+	case ZEROISTHEMOSTCOMMON:
+		return ONEISTHEMOSTCOMMON
+	case ONEISTHEMOSTCOMMON:
+		return ZEROISTHEMOSTCOMMON
+	default:
+		return p
+	}
 }
