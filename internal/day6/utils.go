110625992a76ada7e878b433fcd085619a207290
Juan A. Moyano <wincus@gmail.com>
2021-12-13 17:18:27 -0300

    day6 init work

---
+++ internal/day6/utils.go
@@ -0,0 +1,11 @@
+package day6
+
+import (
+	"github.com/wincus/adventofcode2021/internal/common"
+)
+
+// Solve returns the solutions for day 6
+func Solve(s []string, p common.Part) int {
+
+	return 0
+}
---
+++ internal/day6/utils_test.go
@@ -0,0 +1,34 @@
+package day6
+
+import (
+	"testing"
+
+	"github.com/wincus/adventofcode2021/internal/common"
+)
+
+type Test struct {
+	input []string
+	p     common.Part
+	want  int
+}
+
+func TestSolver(t *testing.T) {
+
+	tests := []Test{
+		{
+			input: []string{
+				"3", "4", "3", "1", "2",
+			},
+			p:    common.Part1,
+			want: 5934,
+		},
+	}
+
+	for _, test := range tests {
+		got := Solve(test.input, test.p)
+
+		if got != test.want {
+			t.Errorf("got %v, want %v for part %v", got, test.want, test.p)
+		}
+	}
+}
