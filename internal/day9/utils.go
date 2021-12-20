8e2d09cc16545b3fe975ecbf6d244a64b671383d
Juan A. Moyano <wincus@gmail.com>
2021-12-20 13:19:34 -0300

    solves day9 part 2

--- internal/day9/utils.go
+++ internal/day9/utils.go
@@ -2,6 +2,7 @@ package day9
 
 import (
 	"log"
+	"sort"
 	"strconv"
 
 	"github.com/wincus/adventofcode2021/internal/common"
@@ -17,15 +18,35 @@ type grid struct {
 func Solve(s []string, p common.Part) int {
 
 	var count int
+	var sizes []int
 
 	g := Parse(s)
 
 	for i, v := range g.field {
-		if g.IsLow(i) {
+		// if i corresponds to a low point, get its value for part 1
+		if g.IsLow(i) && p == common.Part1 {
 			count += v + 1
 		}
+
+		// if i corresponds to a low point, get its basin size for part 2
+		if g.IsLow(i) && p == common.Part2 {
+			pi := new(int)
+			g.getBasinSize(pi, i)
+			sizes = append(sizes, *pi)
+		}
 	}
-	return count
+
+	if p == common.Part1 {
+		return count
+	}
+
+	if p == common.Part2 {
+		sort.Sort(sort.Reverse(sort.IntSlice(sizes)))
+		return sizes[0] * sizes[1] * sizes[2]
+	}
+
+	return 0
+
 }
 
 func Parse(s []string) grid {
@@ -56,7 +77,7 @@ func Parse(s []string) grid {
 
 func (g grid) IsLow(i int) bool {
 
-	ns := g.GetNeighbours(i)
+	ns := g.GetNeighbors(i)
 
 	for _, n := range ns {
 		if g.field[n] <= g.field[i] {
@@ -68,9 +89,9 @@ func (g grid) IsLow(i int) bool {
 
 }
 
-func (g grid) GetNeighbours(i int) []int {
+func (g grid) GetNeighbors(i int) []int {
 
-	var neighbours []int
+	var neighbors []int
 
 	// get row
 	row := i / g.cols
@@ -79,21 +100,45 @@ func (g grid) GetNeighbours(i int) []int {
 
 	// check left
 	if col > 0 {
-		neighbours = append(neighbours, i-1)
+		neighbors = append(neighbors, i-1)
 	}
 	// check right
 	if col < g.cols-1 {
-		neighbours = append(neighbours, i+1)
+		neighbors = append(neighbors, i+1)
 	}
 	// check top
 	if row > 0 {
-		neighbours = append(neighbours, i-g.cols)
+		neighbors = append(neighbors, i-g.cols)
 	}
 	// check bottom
 	if row < g.rows-1 {
-		neighbours = append(neighbours, i+g.cols)
+		neighbors = append(neighbors, i+g.cols)
 	}
 
-	return neighbours
+	return neighbors
 
+}
+
+// getBasinSize is a recursive function that returns the size of a basin
+// starting with an initial low position.
+// NOTE: this function is not thread safe.
+// NOTE 2: this function makes changes on the field is processing, use it
+// with a copy if you need to keep initial values.
+func (g grid) getBasinSize(total *int, position int) {
+
+	if g.field[position] != 9 {
+		*total++              // counts current position
+		g.field[position] = 9 // and avoids counting it again
+	}
+
+	n := g.GetNeighbors(position)
+
+	for _, v := range n {
+
+		if g.field[v] == 9 {
+			continue
+		}
+
+		g.getBasinSize(total, v)
+	}
 }
